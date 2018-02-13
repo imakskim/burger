2b968b6eaf22d811648334660042eaed5a777fe5
Maksim Kim <imakskim@gmail.com>
2018-02-13 21:01:09 +0200

    openMenu rev0

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// // OPEN MENU
+
+// const Menu = document.querySelector ('.header__menu');
+// const trigger = document.querySelector ('.hamburger-menu-link__bars');
+// const TriggerChanging = document.querySelector ('.hamburger-menu-link__bars');
+
+// // trigger.addEventListener ("click", function() {
+// //     Menu.toggleClass ('header__menu--active');
+// // });
+// console.log(trigger);
+
