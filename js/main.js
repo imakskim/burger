a5c2d525ad67ac72fbe0ff43413002a5717d5512
Maksim Kim <imakskim@gmail.com>
2018-02-13 21:27:44 +0200

    openMenuTrigger_bugfixed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-// // OPEN MENU
+const openMenu = document.querySelector ('.header__menu');
+const trigger = document.querySelector ('.hamburger-menu-link__bars');
 
-// const Menu = document.querySelector ('.header__menu');
-// const trigger = document.querySelector ('.hamburger-menu-link__bars');
-// const TriggerChanging = document.querySelector ('.hamburger-menu-link__bars');
-
-// // trigger.addEventListener ("click", function() {
-// //     Menu.toggleClass ('header__menu--active');
-// // });
-// console.log(trigger);
-
+trigger.addEventListener ("click", function() {
+    if (openMenu.classList.contains('header__menu--active')) {
+        openMenu.classList.remove('header__menu--active');
+        trigger.classList.remove('hamburger-menu-link__bars--active')
+    }else {
+        openMenu.classList.add('header__menu--active');
+        trigger.classList.add('hamburger-menu-link__bars--active')
+    }
+});
